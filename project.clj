7e9a4465922c21e12b36d7d248e32a64097c8da5
dnolen <david.nolen@gmail.com>
2010-02-21 15:45:19 -0500

    * project.clj: use Zach's Penumbra from Clojars.

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :description "The Nehe tutorials ported to Penumbra"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.dnolen/penumbra "0.5.0"]
-                 [slick-util "1.0.0"]]
+                 [penumbra "0.5.0"]]
   :native-dependencies [[lwjgl "2.2.2"]]
   :dev-dependencies [[native-deps "1.0.0"]
                      [leiningen/lein-swank "1.1.0"]])
