9d35f47656eb067504be327c8045413976165b1d
dnolen <david.nolen@gmail.com>
2010-02-20 21:01:33 -0500

    * src/clj_nehe/NeHe.bmp: added Nehe Tutorial 6.

---
+++ src/clj_nehe/tutorial6.clj
@@ -0,0 +1,129 @@
+(ns clj-nehe.tutorial6
+  (:use [penumbra opengl geometry]
+        [penumbra.opengl.texture :only [gl-tex-coord-2]]
+        [penumbra.opengl.core :only [gl-import]])
+  (:use [clojure.contrib.duck-streams :only [pwd]])
+  (:require [penumbra.app :as app]))
+
+;; -----------------------------------------------------------------------------
+;; Vars
+
+(def *image-path* (str (pwd) "/src/clj_nehe/NeHe.bmp"))
+(def *width* 640)
+(def *height* 480)
+
+(def *cube*
+     [
+      ; front face
+      [0 0] [-1 -1 1]   ; bottom left of the texture and quad
+      [1 0] [1 -1 1]    ; bottom right of the texture and quad
+      [1 1] [1 1 1]     ; top right of the texture and quad
+      [0 1] [-1 1 1]    ; top left of the texture and quad
+
+      ; back face
+      [1 0] [-1 -1 -1]  ; bottom right of the texture and quad
+      [1 1] [-1 1 -1]   ; top right of the texture and quad
+      [0 1] [1 1 -1]    ; top left of the texture and quad
+      [0 0] [1 -1 -1]   ; bottom left of the texture and quad
+
+      ; top face
+      [0 1] [-1 1 -1]   ; top left of the texture and quad
+      [0 0] [-1 1 1]   ; bottom left of the texture and quad
+      [1 0] [1 1 1]     ; bottom right of the texture and quad
+      [1 1] [1 1 -1]    ; top right of the texture and quad
+
+      ; bottom face
+      [1 1] [-1 -1 -1]  ; top right of the texture and quad
+      [0 1] [1 -1 -1]   ; top left of the texture and quad
+      [0 0] [1 -1 1]    ; bottom left of the texture and quad
+      [1 0] [-1 -1 1]   ; bottom right of the texture and quad
+
+      ; right face
+      [1 0] [1 -1 -1]   ; bottom right of the texture and quad
+      [1 1] [1 1 -1]    ; top right of the texture and quad
+      [0 1] [1 1 1]     ; top left of the texture and quad
+      [0 0] [1 -1 1]    ; bottom left of the texture and quad
+
+      ; left face
+      [0 0] [-1 -1 -1]  ; bottom left of the texture and quad
+      [1 0] [-1 -1 1]   ; bottom right of the texture and quad
+      [1 1] [-1 1 1]    ; top right of the texture and quad
+      [0 1] [-1 1 -1]   ; top left of the texture and quad
+      ])
+
+;; -----------------------------------------------------------------------------
+;; Helpers
+
+(defmacro series [& args]
+  (let [syms (take (count args) (repeatedly gensym))
+        forms (map #(cons 'apply %) (partition 2 (interleave args syms)))]
+   `(fn [[~@syms]]
+      ~@forms)))
+
+(defn tex-coord [x y]
+  (gl-tex-coord-2 x y))
+
+(def tex-coord-and-vertex (series tex-coord vertex))
+
+;; -----------------------------------------------------------------------------
+;; Import
+
+(gl-import glClearDepth clear-depth)
+
+;; -----------------------------------------------------------------------------
+;; Fns
+
+(defn init [state]
+  (app/title! "Nehe Tutorial 6")
+  (app/vsync! false)
+  (app/display-mode! *width* *height*)
+  (enable :texture-2d)
+  (shade-model :smooth)
+  (clear-color 0 0 0 0.5)
+  (clear-depth 1)
+  (enable :depth-test)
+  (depth-test :lequal)
+  (hint :perspective-correction-hint :nicest)
+  (-> state
+      (assoc :xrot 0)
+      (assoc :yrot 0)
+      (assoc :zrot 0)
+      (assoc :texture (load-texture-from-file *image-path*))))
+
+(defn reshape [[x y width height] state]
+  (viewport 0 0 *width* *height*)
+  (frustum-view 45 (/ (double *width*) *height*) 0.1 100)
+  (load-identity)
+  state)
+
+(defn update [[delta time] state]
+   (-> state
+       (update-in [:xrot] #(+ % 0.3))
+       (update-in [:yrot] #(+ % 0.2))
+       (update-in [:zrot] #(+ % 0.4))))
+
+(defn display [[delta time] state]
+  (clear)
+  (load-identity)
+  (translate 0 0 -5)
+  (rotate (:xrot state) 1 0 0)
+  (rotate (:yrot state) 0 1 0)
+  (rotate (:zrot state) 0 0 1)
+  (with-texture (:texture state)
+    (draw-quads
+     (doall
+      (map tex-coord-and-vertex (partition 2 *cube*)))))
+  (app/repaint!))
+
+(defn display-proxy [& args]
+  (apply display args))
+
+(def options {:reshape reshape
+              :update update
+              :display display-proxy
+              :init init})
+
+(defn start []
+  (app/start options {}))
+
+(start)
