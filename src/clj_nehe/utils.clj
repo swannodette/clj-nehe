4a109e661d8d2b64448c5e95ef5830b0e53a927c
dnolen <david.nolen@gmail.com>
2010-04-13 02:39:08 -0400

    * src/clj_nehe/utils.clj: reversed type and fn position. possible to override current type hint

--- src/clj_nehe/utils.clj
+++ src/clj_nehe/utils.clj
@@ -4,25 +4,25 @@
 (defn to-prim-op [expr type]
   (if-let [op (and (coll? expr)
                    (#{'+ '- '* '/ '< '> '=} (first expr)))]
-    (concat `(prim ~op ~type) (rest expr))
+    (concat `(prim ~type ~op) (rest expr))
     expr))
 
 (defmacro prim
   ([type a]
      `(~type ~(to-prim-op a type)))
-  ([fn type a]
+  ([type fn a]
      `(~fn (~type ~(to-prim-op a type))))
-  ([fn type a b]
+  ([type fn a b]
      `(~fn (~type ~(to-prim-op a type)) (~type ~(to-prim-op b type))))
-  ([fn type a b & rest]
+  ([type fn a b & rest]
      `(~fn (~type ~(to-prim-op a type))
-        (prim ~fn ~type ~b ~@rest))))
+        (prim ~type ~fn ~b ~@rest))))
 
 (defmacro prim-int [fn & rest]
-  `(prim ~fn int ~@rest))
+  `(prim int ~fn ~@rest))
 
 (defmacro prim-float [fn & rest]
-  `(prim ~fn float ~@rest))
+  `(prim float ~fn ~@rest))
 
 (comment
 
@@ -31,11 +31,16 @@
   ; < ~20ms
   (dotimes [_ 10]
     (time (dotimes [_ 1000000]
-            (prim + float 4 5 (* 6 7) 8))))
+            (prim float (+ 4 5 (* 6 7) 8)))))
+
+  ; < ~20ms
+  (dotimes [_ 10]
+    (time (dotimes [_ 1000000]
+            (prim float (+ 4 5 (prim int + 1 2))))))
 
   ; ~260ms
   (dotimes [_ 10]
     (time (dotimes [_ 1000000]
             (+ 4 5 (* 6 7) 8))))
-  
+
   )
