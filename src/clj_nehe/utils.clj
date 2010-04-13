64a539c66f73b18448d6a3f21a75a3a90ed7ba8f
dnolen <david.nolen@gmail.com>
2010-04-13 03:15:16 -0400

    * src/clj_nehe/utils.clj: changes to support ->

--- src/clj_nehe/utils.clj
+++ src/clj_nehe/utils.clj
@@ -1,31 +1,52 @@
 (ns clj-nehe.utils
-  (:use [clojure.walk :only [postwalk postwalk-demo]]))
+  (:use [clojure.walk :only [postwalk]]
+        [clojure.contrib.macro-utils :only [mexpand-all]]))
 
-(defn to-prim-op [expr type]
-  (if-let [op (and (coll? expr)
-                   (#{'+ '- '* '/ '< '> '=} (first expr)))]
-    (concat `(prim ~type ~op) (rest expr))
-    expr))
+(defn to-prim-op [type]
+  (fn [expr]
+    (if-let [op (and (coll? expr)
+                     (#{'+ '- '* '/ '< '> '=} (first expr)))]
+      (concat `(prim ~type ~op) (rest expr))
+      expr)))
 
 (defmacro prim
+  "If given an type and expression it will macroexpand the expression fully 
+   and then convert all primitive operations into binary operations and 
+   type-hint all parameters.
+   If given a type, fn, and expression(s) it does not macroexpand all 
+   subexpressions. Basically you have to be somewhat careful how you use 
+   -> in conjuction with prim."
   ([type a]
-     `(~type ~(to-prim-op a type)))
+     `(~type ~(postwalk (to-prim-op type) (mexpand-all a))))
   ([type fn a]
-     `(~fn (~type ~(to-prim-op a type))))
+     `(~fn (~type ~((to-prim-op type) a))))
   ([type fn a b]
-     `(~fn (~type ~(to-prim-op a type)) (~type ~(to-prim-op b type))))
+     `(~fn (~type ~((to-prim-op type) a)) (~type ~((to-prim-op type) b))))
   ([type fn a b & rest]
-     `(~fn (~type ~(to-prim-op a type))
+     `(~fn (~type ~((to-prim-op type) a))
         (prim ~type ~fn ~b ~@rest))))
+
+(defmacro prim-int [fn & rest]
+  `(prim int ~fn ~@rest))
+
+(defmacro prim-float [fn & rest]
+  `(prim float ~fn ~@rest))
 
 (comment
 
   (prim float (+ 4 5 (* 6 7) 8))
-  
+
+  (prim float (-> 4 (+ 5) (+ (* 6 7)) (+ 8)))
+
   ; < ~20ms
   (dotimes [_ 10]
     (time (dotimes [_ 1000000]
             (prim float (+ 4 5 (* 6 7) 8)))))
+
+  ; < ~20ms
+  (dotimes [_ 10]
+    (time (dotimes [_ 1000000]
+            (prim float (-> 4 (+ 5) (+ (* 6 7)) (+ 8))))))
 
   ; < ~20ms
   (dotimes [_ 10]
