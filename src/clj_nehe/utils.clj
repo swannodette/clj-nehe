aa95d90d5eb6c2e6d0ee31acf3ae08aeaf4c45e7
dnolen <david.nolen@gmail.com>
2010-04-13 02:19:12 -0400

    * src/clj_nehe/utils.clj: improving prim

--- src/clj_nehe/utils.clj
+++ src/clj_nehe/utils.clj
@@ -1,12 +1,19 @@
-(ns clj-nehe.utils)
+(ns clj-nehe.utils
+  (:use [clojure.walk :only [postwalk postwalk-demo]]))
+
+(defn to-prim-op [expr type]
+  (if-let [op (and (coll? expr)
+                   (#{'+ '- '* '/ '< '> '=} (first expr)))]
+    (concat `(prim ~op ~type) (rest expr))
+    expr))
 
 (defmacro prim
   ([fn type a]
-     `(~fn (~type ~a)))
+     `(~fn (~type ~(to-prim-op a type))))
   ([fn type a b]
-     `(~fn (~type ~a) (~type ~b)))
+     `(~fn (~type ~(to-prim-op a type)) (~type ~(to-prim-op b type))))
   ([fn type a b & rest]
-     `(~fn (~type ~a)
+     `(~fn (~type ~(to-prim-op a type))
         (prim ~fn ~type ~b ~@rest))))
 
 (defmacro prim-int [fn & rest]
@@ -20,7 +27,7 @@
   ; < ~20ms
   (dotimes [_ 10]
     (time (dotimes [_ 1000000]
-            (prim + float 4 5 (prim * float 6 7) 8))))
+            (prim + float 4 5 (* 6 7) 8))))
 
   ; ~260ms
   (dotimes [_ 10]
