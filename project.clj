38d84d063dc134b99bea5e9ecaa8577f6998b18f
David Nolen <david.nolen@gmail.com>
2010-08-27 19:16:48 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-nehe "0.1.0-SNAPSHOT"
   :description "The Nehe tutorials ported to Penumbra"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [penumbra "0.6.0-SNAPSHOT"]]
   :native-dependencies [[penumbra/lwjgl "2.4.2"]]
-  :dev-dependencies [[native-deps "1.0.1"]
-                     [swank-clojure "1.2.0-SNAPSHOT"]])
+  :dev-dependencies [[native-deps "1.0.3"]
+                     [swank-clojure "1.3.0-SNAPSHOT"]])
